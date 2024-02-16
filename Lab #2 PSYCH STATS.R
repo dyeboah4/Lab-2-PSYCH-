9c4ccfa3a735a8bacc3f435c38aa9358cb82bf3e
dyeboah4 <157760266+dyeboah4@users.noreply.github.com>
2024-02-15 23:15:54 -0500

    Update Lab #2 PSYCH STATS.R

--- Lab #2 PSYCH STATS.R
+++ Lab #2 PSYCH STATS.R
@@ -51,3 +51,9 @@
   > how_many_rolls <- 20
   > sim_rolls <- sample(1:6, how_many_rolls, replace = TRUE) 
   > save.image("~/Downloads/PSYCH STATS Lab # 2 .RData")
+Psych HW # 3 
+Group : Donovan and guy who sits in the front ( I forgot his name I apologize) 
+Disclaimer: I am only running the simulations with one die not two 
+Possible protocol 1 : If the first roll of the dice is a 4 then the dice is unfair. If the first roll of the dice is 1,2,3,5,6 then the dice is fair. Probability of it being a 4 and therefore unfair is ⅙ or 16.7%. Probability of it being either 1,2,3,5,6 is ⅚ or 83.3% 
+Possible protocol 2: I will roll the die 20 times. Out of 20 times the chance that the number 4 is rolled is ⅙ X 20 or 3.33%. If the number 4 comes up more than 3.33% of the time then I will say the dice is unfair. If the number 4 comes up 3.33% of the time or less then the dice is fair. 
+Possible protocol 3: I will roll the die 100 times. Out of 100 times the chance that the number 4 is rolled is ⅙ X 100 or 16.6%. If the number 4 comes up more than 16.6% of the time then I will say the dice is unfair. If the number 4 comes up 16.6% of the time or less then the dice is fair.
